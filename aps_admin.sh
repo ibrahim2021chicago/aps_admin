38d2dcd28d10da48c7dc3529138dadf790506cf0
ibrahim2021chicago <ibrahim2021chicago@gmail.com>
2022-12-22 02:32:50 +0530

    fixed  ps command validation

--- aps_admin.sh
+++ aps_admin.sh
@@ -24,7 +24,11 @@ DR Site: $DRSite
         case $opt in
             "Grep Process")
                 ps_out=`ps -ef | grep '$server-aps-admin' | grep -v grep`;
-                test $? -eq 0 && echo "$ps_out" || echo "Service is not running"
+                if [[ "$ps_out" != "" ]];then
+                    echo "$ps_out"
+                else
+                    echo "Service is not running"
+                fi
 		        echo""
 		        ;;
             "Stop Admin Server")
